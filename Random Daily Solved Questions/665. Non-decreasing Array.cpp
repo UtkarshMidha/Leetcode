e4ca8bce5f03da7032ba94ce56632d64c6c90587
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2022-06-25 19:53:22 +0530

    Create 665. Non-decreasing Array.cpp

---
+++ Random Daily Solved Questions/665. Non-decreasing Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+    if(nums.size()==1) return true;
+      int cnt = 0;                                                                    
+        //the number of changes
+        for(int i = 1; i < nums.size() && cnt<=1 ; i++){
+            if(nums[i-1] > nums[i]){
+                cnt++;
+                if(i-2<0 || nums[i-2] <= nums[i])nums[i-1] = nums[i];                   
+                //modify nums[i-1] of a priority
+                else nums[i] = nums[i-1];                                                
+                //have to modify nums[i]
+            }
+        }
+        return cnt<=1;
+    }
+};
