bcb23622baf7050bfac41eb2f1f7a3a76b4db7f6
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2022-06-30 11:52:49 +0530

    Update 462. Minimum Moves to Equal Array Elements II.cpp
    new approach added

--- Random Daily Solved Questions/462. Minimum Moves to Equal Array Elements II.cpp
+++ Random Daily Solved Questions/462. Minimum Moves to Equal Array Elements II.cpp
@@ -6,12 +6,23 @@ public:
         
         if(nums.size()<2) 
             return 0;
+        // MEDIAN 
+        // int n=nums.size(),ans=0,median=nums[n/2];
+        // for(int i=0;i<n;i++)
+        //     ans+=abs(nums[i]-median);
+        // return ans;
         
-        int n=nums.size(),ans=0,median=nums[n/2];
-        for(int i=0;i<n;i++)
-            ans+=abs(nums[i]-median);
         
-        return ans;
         
+//  --------------------USING DIFFERENCE FROM MID APPROACH :-----------------------
+//         For nums [right], moves = nums [right] - nums [mid]
+//         For nums [left], moves = nums [mid] - nums [left]
+// Hence, adding the above 2 equations, we get:
+//         Total Moves = nums [right] - nums [mid] + nums [mid] - nums [left]
+//         Total Moves = nums [right] - nums [left]
+        int n=nums.size(),mid=n/2,left=0,right=n-1,res=0;
+        while(left<right)
+            res+=nums[right--]-nums[left++];
+        return res;
     }
 };
