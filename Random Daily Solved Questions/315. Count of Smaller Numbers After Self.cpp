64f7ff0e89ecf1823bc01a86968c8dd0992395a3
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2022-07-23 19:34:21 +0530

    Create 315. Count of Smaller Numbers After Self.cpp

---
+++ Random Daily Solved Questions/315. Count of Smaller Numbers After Self.cpp
@@ -0,0 +1,57 @@
+// class Solution {
+// public:
+//     vector<int> countSmaller(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int > ans ;
+//         for(int i=0;i<n-1;i++){
+//             int count=0;
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[j]<nums[i])
+//                     count++;
+//             }
+//             ans.push_back(count);
+//         }
+//         ans.push_back(0);
+//         return ans;
+//     }
+// };
+class Solution {
+public:
+    vector<int> sol;
+    void merge(vector<pair<int,int>> &a,int start,int mid,int end){
+        int i=start,j=mid+1;
+        while(i<=mid && j<=end){
+            if(a[i].first<=a[j].first){
+                sol[a[i].second] += (j-mid-1);
+                i++;
+            }
+            else
+                j++;
+        }
+        while(i<=mid){
+            sol[a[i].second] += (j-mid-1);
+            i++;
+        }
+        sort(a.begin()+start,a.begin()+end+1);
+    }
+    
+    void mergesort(vector<pair<int,int>> &a,int start,int end){
+        if(start>=end) return;
+        int mid = start+(end-start)/2;
+        mergesort(a,start,mid);
+        mergesort(a,mid+1,end);
+        merge(a,start,mid,end);
+    }
+    
+    vector<int> countSmaller(vector<int>& nums) {
+        int n =nums.size();
+        sol.resize(n,0);
+        vector<pair<int,int>> idx;
+
+        for(int i=0;i<n;i++)
+            idx.push_back({nums[i],i});
+        
+        mergesort(idx,0,n-1);
+        return sol;
+    }
+};
