73f030c43f8eac1e2363e957a94fd10643a4e833
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2022-10-21 21:47:44 +0530

    Create 219. Contains Duplicate II.cpp

---
+++ Random Daily Solved Questions/219. Contains Duplicate II.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+  public:
+    // O(n) 
+    bool containsNearbyDuplicate(vector<int>& nums, int k)
+    {
+        unordered_map<int, size_t> map;
+        for (size_t i = 0; i < nums.size(); ++i)
+        {
+            if (map.count(nums[i]))
+            {
+                if (i - map[nums[i]] <= k)
+                    return true;
+            }
+            map[nums[i]] = i;
+        }
+        return false;
+    }
+};
