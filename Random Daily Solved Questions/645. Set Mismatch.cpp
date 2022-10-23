25224713589c77edba1030d8de86c00b85cba991
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2022-10-23 09:51:00 +0530

    Create 645. Set Mismatch.cpp

---
+++ Random Daily Solved Questions/645. Set Mismatch.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+    vector<int> ans (2,0);
+        for(int i = 0; i < nums.size(); i++) {
+            int val = abs(nums[i]);
+            ans[1] ^= (i+1) ^ val;
+            if (nums[val-1] < 0) ans[0] = val;
+            else nums[val-1] = -nums[val-1];
+        }
+        ans[1] ^= ans[0];
+        return ans;
+        
+    }
+};
