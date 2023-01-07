02d8d4caf97c46085e942877f92f1792893c8878
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2023-01-07 09:20:30 +0530

    Create 134. Gas Station.cpp

---
+++ Random Daily Solved Questions/134. Gas Station.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n=gas.size(),total_fuel(0),total_cost(0);
+        for(int i=0;i<n;i++){
+            total_cost+=cost[i];
+            total_fuel+=gas[i];
+        }
+        if(total_fuel<total_cost) return -1;
+    int curr_fuel(0),start(0);
+    for(int i=0;i<n;i++){
+        if(curr_fuel<0){
+            start=i;
+            curr_fuel=0;
+        }
+        
+        curr_fuel+=gas[i]-cost[i];
+    }
+    return start;
+    
+    
+    }
+};
