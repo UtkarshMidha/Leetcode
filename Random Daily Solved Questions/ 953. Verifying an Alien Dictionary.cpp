4b482c39b3ebc35ea8ebe7176c45ea07a158c602
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2023-02-02 20:30:31 +0530

    Update  953. Verifying an Alien Dictionary.cpp

--- Random Daily Solved Questions/ 953. Verifying an Alien Dictionary.cpp
+++ Random Daily Solved Questions/ 953. Verifying an Alien Dictionary.cpp
@@ -1,4 +1,6 @@
-unordered_map<char, int> mp;
+class Solution {
+public:
+    unordered_map<char, int> mp;
 bool isAlienSorted(vector<string>& words, string order) {        
 	for(int i = 0; i < size(order); i++) mp[order[i]] = i;  // mapping letter to its corresponding index in order
 	for(int i = 0; i < size(words) - 1; i++)
@@ -12,3 +14,4 @@ bool checkOrder(string& a, string& b){
 		if(mp[a[i]] != mp[b[i]]) return mp[a[i]] < mp[b[i]];
 	return size(a) <= size(b);
 }
+};
