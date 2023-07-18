0ff7cd09ec60e4a6eacc057ea5fd946c9d824ac8
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2023-07-18 19:07:20 +0530

    Update  146. LRU Cache.cpp

--- Random Daily Solved Questions/ 146. LRU Cache.cpp
+++ Random Daily Solved Questions/ 146. LRU Cache.cpp
@@ -1,32 +1,35 @@
-class LRUCache{
-    size_t m_capacity;
-    unordered_map<int,  list<pair<int, int>>::iterator> m_map; //m_map_iter->first: key, m_map_iter->second: list iterator;
-    list<pair<int, int>> m_list;                               //m_list_iter->first: key, m_list_iter->second: value;
-public:
-    LRUCache(size_t capacity):m_capacity(capacity) {
-    }
-    int get(int key) {
-        auto found_iter = m_map.find(key);
-        if (found_iter == m_map.end()) //key doesn't exist
-            return -1;
-        m_list.splice(m_list.begin(), m_list, found_iter->second); //move the node corresponding to key to front
-        return found_iter->second->second;                         //return value of the node
-    }
-    void set(int key, int value) {
-        auto found_iter = m_map.find(key);
-        if (found_iter != m_map.end()) //key exists
+class LRUCache
+{
+    public:
+        list<pair<int,int>> l;
+        unordered_map<int,list<pair<int, int>>::iterator> m;
+        int size;
+        LRUCache(int capacity)
         {
-            m_list.splice(m_list.begin(), m_list, found_iter->second); //move the node corresponding to key to front
-            found_iter->second->second = value;                        //update value of the node
-            return;
+            size=capacity;
         }
-        if (m_map.size() == m_capacity) //reached capacity
+        int get(int key)
         {
-           int key_to_del = m_list.back().first; 
-           m_list.pop_back();            //remove node in list;
-           m_map.erase(key_to_del);      //remove key in map
+            if(m.find(key)==m.end())
+                return -1;
+            l.splice(l.begin(),l,m[key]);
+            return m[key]->second;
         }
-        m_list.emplace_front(key, value);  //create new node in list
-        m_map[key] = m_list.begin();       //create correspondence between key and node
-    }
+        void put(int key, int value)
+        {
+            if(m.find(key)!=m.end())
+            {
+                l.splice(l.begin(),l,m[key]);
+                m[key]->second=value;
+                return;
+            }
+            if(l.size()==size)
+            {
+                auto d_key=l.back().first;
+                l.pop_back();
+                m.erase(d_key);
+            }
+            l.push_front({key,value});
+            m[key]=l.begin();
+        }
 };
