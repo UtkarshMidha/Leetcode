8715ad8996d0acdc19083bf0c1c6234c2c5c2b4d
Utkarsh Midha <72035227+UtkarshMidha@users.noreply.github.com>
2023-08-01 20:14:33 +0530

    Create   77. Combinations.cpp

---
+++ Random Daily Solved Questions/77. Combinations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+       void backtrack(vector<vector<int>> &result , vector<int> current, int pos, int previous, int n, int k) {
+           if(pos==k){
+               result.push_back(current);
+                return;
+               }
+           for(int i=previous+1;i<=n;i++){
+               current.push_back(i);
+               backtrack(result, current, pos+1, i, n, k);
+               current.pop_back();
+           }
+       }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> current;
+        backtrack(result, current, 0, 0, n, k);
+        return result; 
+    }
+};
